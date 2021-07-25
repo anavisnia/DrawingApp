aca9174b41578b63d472e7b0e43c70c981c1f9f6
Ana Višnevskaja <anavisnia@gmail.com>
2021-07-25 22:04:15 +0300

    Add project files.

---
+++ DrawingApp/Hubs/DrawingHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace DrawingApp.Hubs
+{
+    public class DrawingHub : Hub
+    {
+        public async Task SendCoord(int x, int y)
+        {
+            await Clients.Others.SendAsync("receiveCoord", x, y);
+        }
+    }
+}
